3503ca34007ef60df82a4197f71b24bd0a7fb50d
Stacey Smich <ssmich@Staceys-MacBook-Pro.local>
2019-06-24 15:37:30 -0600

    Commit 2 - Get Even answered

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@
 for (i = 1; i <= 20; i++){
 	console.log(i);
 }
+
+// Get Even
+// Write a for loop that will log only the even numbers 
+// in 0 through 200.
+
+for (i = 0; i<= 200; i++){
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+}
