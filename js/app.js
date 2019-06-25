dfa71dbec606e12420dbe9473dd7da163441131d
Stacey Smich <ssmich@Staceys-MacBook-Pro.local>
2019-06-25 14:17:02 -0600

    Gettig to Know You answered

--- js/app.js
+++ js/app.js
@@ -54,3 +54,60 @@ for (i = 1; i <= 100; i++){
 	};
 }
 
+// Getting to Know You 
+// Use the following arrays to answer the questions below (name, age, 
+// hometown):
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+// 	1.	Jim Clark decides that Kenny can't be named "Kenny" anymore. 
+//		Remove "Kenny" from the kenny array and replace it with 
+//		"Gameboy".
+
+kenny[0] = "Gameboy";
+console.log(kenny[0]);
+
+// 	2.	Jim Clark just had his birthday; change jimClark's array to 
+//		reflect Jim being a year older. Don't just hard code 187-
+//		-pretend that you didn't already know that his age is 186, 
+//		and write your code to just makke him a year older than 
+//		whatever age he was.
+
+jimClark[1] = jimClark[1] + 1;
+console.log(jimClark[1]);
+
+// 	3. 	Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown 
+//		from "Denver" to "Gotham City".
+
+ryan[2] = "Gotham City";
+console.log(ryan[2]);
+
+// 	4.	Reuben left Durham 5 years ago to come to Chicago. First, 
+//		remove "Durham" from Reuben's array, and then add "Chicago". 
+//		(Note: remove and then add is different from simply changing 
+//		the value at that index.)
+
+reuben.pop();
+reuben.push("Chicago");
+console.log(reuben);
+
+// 	5.	Jim Haff could be literally anywhere in the world. Remove "All 
+// 		cities" from his array, then pick any 3 cities you like, and 
+//		add them to Jim's array. If you did it in 3 lines of code that's 
+//		fine, but see if you can do it in one line of code.
+
+jimHaff.pop();
+jimHaff.push("Orlando, FL","Somerset, Bermuda", 
+	"Anna Maria Island, FL");
+console.log(jimHaff);
+
+// 	Bonus: Whoops! Jim Haff is actually only allowed to be in one of 
+// 	two cities. Whatever the first of the 3 cities for Jim Haff is now, 
+// 	remove it from the array using .splice()
+
+jimHaff.splice(2,1);
+console.log(jimHaff);
+
